11713b919246998a4acc7a57301a4ed20ab44172
shvilaine <severine.vilaine@gmail.com>
2024-09-25 17:24:40 +0200

    Added an article component in the shape of a card

---
+++ src/app/ArticleComponent/article.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.scss'
+})
+
+export class ArticleComponent {
+
+}
