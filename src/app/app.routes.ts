919247e7c432a944f775c9340a942b83cec7450e
shvilaine <severine.vilaine@gmail.com>
2024-10-01 17:18:37 +0200

    Made a Reactive form for the login/subscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent}, 
     {path: 'article/:id', component: ArticlePageComponent},
     {path: 'contact', component: ContactFormComponent},
+    {path: 'login', component: SignupFormComponent},
     {path: '**', component: NotFoundComponent},
 ];
